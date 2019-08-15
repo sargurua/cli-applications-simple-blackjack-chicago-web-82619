48019a946c8ad8fc9f61105d6fcf808b02ba87cb
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-15 20:38:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -16,11 +16,11 @@ end
 def prompt_user
   # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
-  gets
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
 def end_game

782646671522b1e1ca436ada61ec4c0ab3551efc
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-15 20:37:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,9 +8,9 @@ def deal_card
   rand(1...11)
 end
 
-def display_card_total(total)
+def display_card_total(card_total)
   # code #display_card_total here
-  puts total
+  puts 
 end
 
 def prompt_user
